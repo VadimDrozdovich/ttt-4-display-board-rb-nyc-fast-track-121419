00a5c5a560e9943388992a74be2eb1de188d9f9b
Vadym Drozdovych <vadim.drozdovich@gmail.com>
2019-12-14 00:32:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,5 @@
-# Define display_board that accepts a board and prints
+board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
+def display_board(board)
+  puts board
+end# Define display_board that accepts a board and prints
 # out the current state.
